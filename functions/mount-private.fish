08b4c6764d5ee6df0d79ac9673ec43d64e241058
Aphyr <aphyr@aphyr.com>
2015-02-02 20:41:06 -0800

    deleted mount-private

--- functions/mount-private.fish
+++
@@ -1,6 +0,0 @@
-function mount-private
-  sudo mount ~/private ~/private -t \
-    ecryptfs -o key=passphrase,ecryptfs_cipher=aes,ecryptfs_key_bytes=24,\
-    ecryptfs_passthrough=no,ecryptfs_enable_filename_crypto=yes,\
-    no_sig_cache=yes
-end
