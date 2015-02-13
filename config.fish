454c2a775fda915b61e5ff6e9b45e7cba59bc45e
Aphyr <aphyr@aphyr.com>
2015-02-12 22:22:32 -0800

    config.fish updates

--- config.fish
+++ config.fish
@@ -1,12 +1,22 @@
 set fish_greeting ""
 
 # Paths
-set PATH $HOME/bin $PATH
 set PATH $HOME/.rvm/bin $PATH
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/stripe/henson/bin $PATH
+set PATH $HOME/stripe/password-vault/bin $PATH
+set PATH $HOME/stripe/space-commander/bin $PATH
+set PATH $HOME/bin $PATH
 
 # Editors
 set -x EDITOR "/usr/bin/vim"
 set -x VISUAL "/usr/bin/vim"
+
+# rbenv
+if type rbenv > /dev/null
+  set -gx RBENV_ROOT $HOME/.rbenv
+  . (rbenv init -|psub)
+end
 
 if status --is-interactive
   # Keybinds
@@ -14,5 +24,5 @@ if status --is-interactive
   bind \c\[1\;5D backward-word
 
   # Load SSH Keys
-  eval (keychain --eval --quiet -Q id_rsa | tr \n \;)
+  # eval (keychain --eval --quiet -Q id_rsa | tr \n \;)
 end
